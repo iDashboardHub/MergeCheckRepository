f0f4920ba525d2b573d1416b5ae3c199e6050e01
iDashboardemouser <Prakash.Thangasamy@cognizant.com>
2017-07-20 05:42:21 +0000

    Initial commit to local repository

---
+++ MergeProject/src/ca/demo/mergeproject/Main.java
@@ -0,0 +1,14 @@
+package ca.demo.mergeproject;
+
+public class Main {
+	public static void main(String[] args){
+		/*Test data = new Test();*/
+		displayGreeting();
+		/*data.display();*/
+	}
+
+	private static void displayGreeting(){
+	System.out.println("Hello");
+	
+	}
+}
